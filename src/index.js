ccfbc3268837a9329ecfa395bb53abdabf8275f9
vlavirus <androando777@gmail.com>
2020-03-21 22:45:19 +0200

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let answer = [];
+
+    for ( let currentItem of str ) {
+      let[starItem, endItem] = bracketsConfig.find(pair =>
+        pair.includes(currentItem));
+
+      if ( starItem === currentItem && !answer.includes(starItem) ) {
+        answer.push(endItem);
+        continue;
+      }
+
+      let checkItem = answer.pop();
+      if ( currentItem === checkItem ) {
+        continue;
+      }
+      return false;
+    }
+    // console.log('test');
+  return !answer.length;
+};
